4ad0a8935a923d018c0d6de95db847ecc1a925f8
macbook <harisgorcevic1000@gmail.com>
2024-01-13 23:55:48 +0100

    login register

--- src/SignUpFolder/SignUp.js
+++ src/SignUpFolder/SignUp.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./signup.css";
+import { useNavigate } from "react-router-dom";
 export default function SignUp() {
+  const navigate = useNavigate();
   return (
     <div className="Big">
       <div className="Logincontainer">
@@ -32,11 +34,15 @@ export default function SignUp() {
               name="password"
             />
           </div>
-          <button type="submit">Sign up </button>
+          <button onSubmit={() => navigate("/")} type="submit">
+            Sign up{" "}
+          </button>
           <p style={{ paddingLeft: "20px" }}>
             You are agree to aour terms and policies
           </p>
-          <button type="button">Login</button>
+          <button onClick={() => navigate("/")} type="button">
+            Login
+          </button>
         </form>
       </div>
     </div>
