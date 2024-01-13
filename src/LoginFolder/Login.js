93542e7bb6ab7dfe7615c3a61f949667c3a098a2
macbook <harisgorcevic1000@gmail.com>
2024-01-14 00:33:16 +0100

    validacije

--- src/LoginFolder/Login.js
+++ src/LoginFolder/Login.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import { useNavigate } from "react-router-dom";
+import Validation from "../Validation/LoginValidation";
 export default function Login() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleInputSubmit(event) {
+    event.preventDefault();
+    setValues(Validation(values));
+  }
+
+  function handleInput(event) {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  }
+
   const navigate = useNavigate();
   return (
     <div className="Big">
@@ -12,21 +30,13 @@ export default function Login() {
             <strong>
               <label>Email:</label>
             </strong>
-            <input
-              type="email"
-              placeholder="Enter your email adress"
-              name="email"
-            />
+            <input type="email" placeholder="Enter your email adress" />
           </div>
           <div className="form-group">
             <strong>
               <label>Password:</label>
             </strong>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              name="password"
-            />
+            <input type="password" placeholder="Enter your password" />
           </div>
           <button onClick={() => navigate("/signup")} type="submit">
             Sign up{" "}
